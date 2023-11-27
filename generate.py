afff07752001968ffac0898295907ff9885e22b0
cronoimpius <gianmi.porrazzo@gmail.com>
2023-11-27 12:18:56 +0100

    Testing with llama

--- generate.py
+++ generate.py
@@ -1,11 +1,14 @@
 import llama_cpp
 
 # Initialize Llama object
-llm = llama_cpp.Llama(model_path="./model/llama-2.gguf", n_ctx=512,n_batch=128)
+llm = llama_cpp.Llama(
+    model_path="./model/llama-2-7b-chat.Q5_K_M.gguf", 
+    n_ctx=512
+)
 
 # Generate random data
 #random_data = llm("Q: Generate random data for an ldap server. make sure to print a .schema file and the correspondant .ldif", max_tokens=32, stop=["Q:", "\n"], echo=True)
-prompt = "Using the ldif syntax, generate 10 entries for an ldap server for the organization example.com"
+prompt = "Generate 10 entries for an ldap server using ldif syntax"
 
 output = llm(prompt,temperature=1,top_p=0.9,max_tokens=-1)
 # Print generated data
