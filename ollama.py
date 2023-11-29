3dc7c21d3fc167954d0e38ae21a0e11df4ce1d25
cronoimpius <gianmi.porrazzo@gmail.com>
2023-11-29 18:13:07 +0100

    Report write, ENV variables in dockerfile ollama.py to generate data TODO: finish report, clean generated data, make screenshot, install ollama in docker

---
+++ ollama.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+# vim:fenc=utf-8
+#
+# Copyright © 2023 t14 <t14@t14>
+#
+# Distributed under terms of the MIT license.
+
+"""
+Use ollama in a scipt
+"""
+
+from langchain.llms import Ollama
+olama= Ollama(model="llama2")
+out=olama("generate 10 entry for ldap server using ldif syntax")
+with open("response.txt", "a") as f:
+  f.write(out)
+
